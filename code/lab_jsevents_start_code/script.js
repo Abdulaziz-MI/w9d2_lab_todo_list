93e51c8ef8757ecfca0a1bb41e091060867a63d9
Abdulaziz-MI <am.alzaid@outlook.com>
2023-08-29 16:09:21 +0100

    collected input value

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -4,17 +4,22 @@ const list = document.querySelector('#list');
 const input = document.querySelector('#new-todo')
 const enterBtn = document.querySelector('#enter')
 
+input.addEventListener("input", (event) => {
+    list.lastChild = event.target.value;
+});
+
 const handleSubmit = (event) => {
     event.preventDefault();
-    const newToDo = toDoForm.target.value;
+    const newToDo = input.value;
     console.log(newToDo);
     const newListItem = document.createElement('li');
     newListItem.innerText = newToDo;
     list.appendChild(newListItem);
-
 }
+
 
 console.log(toDoForm);
 
 toDoForm.addEventListener('submit', handleSubmit);
+
 enterBtn.addEventListener('click', handleSubmit);
