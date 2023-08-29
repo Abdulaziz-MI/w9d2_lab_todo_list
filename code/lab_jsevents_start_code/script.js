2750adabe5cfde6c928976c7b8cf66f60599c102
Sandra <sandramtzd@hotmail.com>
2023-08-29 22:03:52 +0100

    changed newToDo definition and disabling button listener

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -4,22 +4,23 @@ const list = document.querySelector('#list');
 const input = document.querySelector('#new-todo')
 const enterBtn = document.querySelector('#enter')
 
-input.addEventListener("input", (event) => {
-    list.lastChild = event.target.value;
-});
+// input.addEventListener("input", (event) => {
+//     list.lastChild = event.target["new-todo"].value;//our solution: event.target.value;
+// });
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    const newToDo = input.value;
+    const newToDo = event.target["new-todo"].value;// original: toDoForm.target | our solution: input.value;
     console.log(newToDo);
     const newListItem = document.createElement('li');
     newListItem.innerText = newToDo;
     list.appendChild(newListItem);
+    event.target.reset();
 }
 
 
 console.log(toDoForm);
 
 toDoForm.addEventListener('submit', handleSubmit);
+//enterBtn.addEventListener('click', handleSubmit);
 
-enterBtn.addEventListener('click', handleSubmit);
